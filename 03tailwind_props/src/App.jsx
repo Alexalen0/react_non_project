e11e315c373d46a68ac1d639d6319f8bd967789c
Alexalen0 <alex3alen@gmail.com>
2024-02-20 00:04:55 +0530

    Addition

--- 03tailwind_props/src/App.jsx
+++ 03tailwind_props/src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  let myobjj = {
+    username: "Alex",
+    age: 21
+  }
+  let arr = [1,2,3,4]
   return (
     <>
-      <h1 className='bg-green-400'>Tailwind test</h1>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      {/*<Card username="Aklex" someobj = {myobjj} narr = {arr}/>*/}
+      <Card username="Jiffin" btntxt="Click Me"/>
+      <Card username="Alex" btntxt="Visit Me"/>
     </>
   )
 }
